7da9454688774f12885f0e320d3406307a714193
Sean Dillon <pdillon0@gmail.com>
2022-08-29 14:32:24 -0400

    routes, models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,28 @@
 const User = require('./User');
+const Project = require('./Project');
+const Comment = require('./Comment');
 
-module.exports = { User };
+User.hasMany(Project, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Project, {
+    foreignKey: 'project_id'
+});
+
+
+module.exports = { User, Project, Comment };
