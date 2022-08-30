06d6f1bea57768a0659d609d5d0e1188379875f1
Sean Dillon <pdillon0@gmail.com>
2022-08-30 12:14:17 -0400

    models and relationships

---
+++ controllers/api/projectRoutes.js
@@ -0,0 +1,3 @@
+const router = require('express').Router();
+const { Project } = require('../../models');
+const withAuth = require('../../utils/auth');
--- models/index.js
+++ models/index.js
@@ -1,25 +1,29 @@
 const User = require('./User');
 const Project = require('./Project');
 const Comment = require('./Comment');
-
+// A user can own many projects
 User.hasMany(Project, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
-
+// A user can have many comments
 User.hasMany(Comment, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
-
+// A project is always owned by one user
 Project.belongsTo(User, {
     foreignKey: 'user_id'
 });
-
+// A project can reference many users as a collaborator
+Project.hasMany(User, {
+    foreignKey: 'collaborator'
+});
+// A comment always belongs to one user
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
 });
-
+// A comment always belongs to one project
 Comment.belongsTo(Project, {
     foreignKey: 'project_id'
 });
