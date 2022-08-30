ce10fef832893a2dd9821628a403a1a39520efa8
Juliet Lamond <jlamond37@Juliets-MacBook-Pro.local>
2022-08-30 13:40:47 -0400

    file structure, able to render images

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
-const { Project, Comment, User } = require('../models');
-const withAuth = require('../utils/auth');
+// const { Project, Comment, User } = require('../models');
+// const withAuth = require('../utils/auth');
+
+router.get("/",(req, res) => {
+res.render("profilepage")
+})
+module.exports=router
