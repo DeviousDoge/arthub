4d3753fa0b678a04f782f62582fd362e43cc9f3a
Sean Dillon <pdillon0@gmail.com>
2022-08-31 12:33:30 -0400

    saving

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,51 @@
 const router = require('express').Router();
-// const { Project, Comment, User } = require('../models');
-// const withAuth = require('../utils/auth');
+const { Project, Comment, Collaborator, User } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get("/",(req, res) => {
-    res.render("profilepage")
-})
+router.get("/", (req, res) => {
+    try {
+        // Get all projects and JOIN with user data
+        const projectData = await Project.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
 
-module.exports=router
+        // Serialize data so the template can read it
+        const projects = projectData.map((project) => project.get({ plain: true }));
+
+
+        res.render("profilepage", {
+            //   projects, 
+            //   logged_in: req.session.logged_in 
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/project/:id', async (req, res) => {
+    try {
+        const projectData = await Project.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const project = projectData.get({ plain: true });
+
+        res.render('commentpage', {
+            ...project,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+module.exports = router
