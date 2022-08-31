5e836601d853040c5bb408d17df1d92f430efec8
Sean Dillon <pdillon0@gmail.com>
2022-08-31 18:12:55 -0400

    frontend javascript

---
+++ public/js/profile.js
@@ -0,0 +1,49 @@
+//using placeholder elements as i do not know the classnames of these textbox and button elements
+
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#project-name').value.trim();
+    const description = document.querySelector('#project-desc').value.trim();
+  
+    if (title && description) {
+      const response = await fetch(`/api/projects`, {
+        method: 'POST',
+        body: JSON.stringify({ title, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create project');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/projects/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-project-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.project-list')
+    .addEventListener('click', delButtonHandler);
+  
