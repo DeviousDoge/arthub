c6c0075e9a7247d64a4aabbf530f1dc9e4c458e0
Sean Dillon <pdillon0@gmail.com>
2022-08-31 13:45:19 -0400

    server.js and seeds.js now work

--- models/Comment.js
+++ models/Comment.js
@@ -35,14 +35,14 @@ Comment.init(
           key: 'id',
         },
       },
-  
-  
+    },
+  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'comment',
-    }
+  }
 );
 
 module.exports = Comment;
