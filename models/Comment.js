f2a9580f1c77668d8b7646eeb04f42bdb9294e10
idakukimiya <kukimiya.ida@gmail.com>
2022-08-30 23:35:26 -0400

    comment controller coding

--- models/Comment.js
+++ models/Comment.js
@@ -11,10 +11,6 @@ Comment.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
     description: {
       type: DataTypes.STRING,
     },
