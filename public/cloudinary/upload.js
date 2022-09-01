4068ea2587516de9b51fdf062287308c638c9a52
idakukimiya <kukimiya.ida@gmail.com>
2022-09-01 13:11:17 -0400

    test cloudinary

--- public/cloudinary/upload.js
+++ public/cloudinary/upload.js
@@ -6,27 +6,27 @@ const cloudinary = require('cloudinary').v2;
 //////
 // Basic Upload Method
 //////
-// cloudinary.uploader.upload("./assets/cat.jpg")
-// 	.then(result => {console.log(result)})
-// 	.catch(error => {console.log(error)});
+cloudinary.uploader.upload("./assets/cat.jpg")
+	.then(result => {console.log(result)})
+	.catch(error => {console.log(error)});
 
 
 //////
 // Resource Type
 //////
-// cloudinary.uploader.upload("./assets/dog.webm", {resource_type: "auto"})
-// 	.then(result => {console.log(result)})
-// 	.catch(error => {console.log(error)});
+cloudinary.uploader.upload("./assets/dog.webm", {resource_type: "auto"})
+	.then(result => {console.log(result)})
+	.catch(error => {console.log(error)});
 
 
-// cloudinary.uploader.upload("./assets/dog.mp4", {resource_type: "video", public_id: "dog"})
-// 	.then(result => {console.log(result)})
-// 	.catch(error => {console.log(error)});
+cloudinary.uploader.upload("./assets/dog.mp4", {resource_type: "video", public_id: "dog"})
+	.then(result => {console.log(result)})
+	.catch(error => {console.log(error)});
 
-// cloudinary.uploader.upload("./assets/BLKCHCRY.TTF",
-// 	{resource_type: "raw", use_filename: true, unique_filename: false})
-// 	.then(result => {console.log(result)})
-// 	.catch(error => {console.log(error)});
+cloudinary.uploader.upload("./assets/BLKCHCRY.TTF",
+	{resource_type: "raw", use_filename: true, unique_filename: false})
+	.then(result => {console.log(result)})
+	.catch(error => {console.log(error)});
 
 
 //////
@@ -58,18 +58,18 @@ const cloudinary = require('cloudinary').v2;
 //////
 // Upload Source Options
 //////
-// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/5/56/Chocolate_cupcakes.jpg")
-// 	.then(result => {console.log(result)})
-// 	.catch(error => {console.log(error)});
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/5/56/Chocolate_cupcakes.jpg")
+	.then(result => {console.log(result)})
+	.catch(error => {console.log(error)});
 
-// cloudinary.uploader.upload("s3://my-bucket/my-path/example.jpg")
-// 	.then(result => {console.log(result)})
-// 	.catch(error => {console.log(error)});
+cloudinary.uploader.upload("s3://my-bucket/my-path/example.jpg")
+	.then(result => {console.log(result)})
+	.catch(error => {console.log(error)});
 
-// cloudinary.uploader.upload("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQYV2NgYAAAAAMAAWgmWQ0AAAAASUVORK5CYII=")
-// 	.then(result => {console.log(result)})
-// 	.catch(error => {console.log(error)});
+cloudinary.uploader.upload("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQYV2NgYAAAAAMAAWgmWQ0AAAAASUVORK5CYII=")
+	.then(result => {console.log(result)})
+	.catch(error => {console.log(error)});
 
-// cloudinary.uploader.upload("ftp:user1:mypass@ftp.example.com/sample.jpg")
-// 	.then(result => {console.log(result)})
-// 	.catch(error => {console.log(error)});
+cloudinary.uploader.upload("ftp:user1:mypass@ftp.example.com/sample.jpg")
+	.then(result => {console.log(result)})
+	.catch(error => {console.log(error)});
