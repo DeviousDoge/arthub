ccb7908f0bf204b0d1dc223ca421e9b1410b576c
Sean Dillon <pdillon0@gmail.com>
2022-09-01 17:16:35 -0400

    saving

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,4 +1,17 @@
 //using placeholder elements as i do not know the classnames of these textbox and button elements
+var myWidget = cloudinary.createUploadWidget({
+  cloudName: 'ikukimxb',
+  uploadPreset: 'my_preset'
+}, (error, result) => {
+  if (!error && result && result.event === "success") {
+    console.log('Done! Here is the image info: ', result.info.url);
+  }
+}
+)
+
+document.getElementById("upload_widget").addEventListener("click", function () {
+  myWidget.open();
+}, false);
 
 const newFormHandler = async (event) => {
     event.preventDefault();
